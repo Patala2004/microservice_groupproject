6c9adb584d7ec21273f5b93e113e0cbd1f627d0b
Patala2004 <christian.most@hotmail.com>
2025-12-07 18:02:10 +0800

    FIX: Gateway double or zero cors headesrs bug

--- api_gateway/api-gateway/src/main/java/microservices/gateway/api_gateway/ApiGatewayApplication.java
+++ api_gateway/api-gateway/src/main/java/microservices/gateway/api_gateway/ApiGatewayApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -26,6 +24,7 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
+    
 
     @Bean
     public OncePerRequestFilter schemaModificationFilter() {
