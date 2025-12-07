2876468c762f97944971cc11b590d638b1b1d51c
pcl <sofia.canogar@alumnos.upm.es>
2025-12-08 02:27:24 +0800

    add ollama_llm module

--- tag/tag/main.py
+++ tag/tag/main.py
@@ -1,7 +1,44 @@
 from fastapi import FastAPI
+import ollama_llm as llm
 
 app = FastAPI()
+phi3 = llm.Phi3_3b()
+qwen4b = llm.Qwen3_4b()
+qwen8b = llm.Qwen3_8b()
 
 @app.get("/")
 def root():
     return {"message": "¡Hola FastAPI!"}
+
+@app.post("/llm-test/phi3")
+def llm_test_phi3():
+    llm_test(phi3)
+
+@app.post("/llm-test/qwenb4")
+def llm_test_qwenb4():
+    llm_test(qwen4b)
+
+@app.post("/llm-test/qwenb8")
+def llm_test_qwenb8():
+    llm_test(qwen8b)
+
+def llm_test(llm: llm.OllamaLLM):
+    return [llm_test_1(llm), llm_test_2(llm), llm_test_3(llm)]
+
+def llm_test_1(llm: llm.OllamaLLM):
+    return llm.generate_tags(
+        input_title="Basketball match",
+        input_content="We would want to play a basketball match tomorrw at 8 pm. We still need 4 extra people. Please join in!"
+    )
+
+def llm_test_2(llm: llm.OllamaLLM):
+    return llm.generate_tags(
+        input_title="Selling fridge",
+        input_content="Hi, I'm selling a 66L capacity fridge with a 22L capacity freezer for ¥200. If anyone is interested, please contact me before the end of this month."
+    )
+
+def llm_test_3(llm: llm.OllamaLLM):
+    return llm.generate_tags(
+        input_title="Zootropolis in Anting cinema this Saturday",
+        input_content="We would like to go to the cinema in Anting this Saturday to watch Zootropolis 2. If someone else also wants to come we could share transport expenses."
+    )
---
+++ tag/tag/ollama_llm.py
@@ -0,0 +1,59 @@
+from ollama import Client
+import os
+
+class OllamaLLM:
+    _shared_client = None
+
+    def __init__(self, model_name = ""):
+        if OllamaLLM._shared_client == None:
+            OllamaLLM._shared_client = Client(
+                host=os.environ['OLLAMA_HOST'],
+                headers={}
+            )
+        self.client = OllamaLLM._shared_client
+        self.model_name = model_name
+    
+    def generate_tags(self, input_title, input_content):
+        response = self.client.generate(
+            prompt = get_prompt(input_title, input_content),
+            model = self.model_name
+        )
+        return response_to_tagnames_list(response)
+
+class Phi3_3b(OllamaLLM):
+    def __init__(self):
+        super().__init__(model_name = "phi3:3.8b-mini-4k-instruct-q2_K")
+
+class Qwen3_4b(OllamaLLM):
+    def __init__(self):
+        super().__init__(model_name = "qwen3:4b")
+
+class Qwen3_8b(OllamaLLM):
+    def __init__(self):
+        super().__init__(model_name = "qwen3:8b")
+
+
+def get_prompt(input_title: str, input_content: str):
+    return (
+        f"""Task: Extract between 1 and 15 concepts that represent the main topics of the Title and Content.
+        Extraction rules (strict):
+        - Concepts must be nouns or noun phrases.
+        - Concepts must represent the main subjects or high-level themes (e.g., activity, domain, object, event, topic).
+        - Concepts must be explicitly present or clearly implied by the Title or Content.
+        - Ignore all secondary details, including: time, date, location, people, invitations, metadata, opinions, logistics, prices, or any other incidental information.
+        - Do not infer or invent information not grounded in the text.
+        - Do not repeat concepts.
+        - Output strictly in the format: <concept>$<concept>$...<concept> (with $ and no spaces).
+        - Output only the concepts.
+
+        Input:
+        Title: {input_title}
+        Content: {input_content}
+        """
+    )
+
+def response_to_tagnames_list(response: str):
+    return response.split("$")
+
+
+
