6929ba6873f7528563a19c45dfcda4e19d166f5b
Pablo Macías <pablomaciasbrad@gmail.com>
2025-12-07 21:46:32 +0800

    consumer functionality

--- user-preferences/user-pref/src/main/java/group5/ms/tongji/upref/config/BrokerQueueConfig.java
+++ user-preferences/user-pref/src/main/java/group5/ms/tongji/upref/config/BrokerQueueConfig.java
@@ -1,2 +1,33 @@
-package group5.ms.tongji.upref.config;public class BrokerQueueConfig {
+package group5.ms.tongji.upref.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BrokerQueueConfig {
+    @Bean
+    public Queue interQueue() {
+        return new Queue("inter.queue", true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            Jackson2JsonMessageConverter messageConverter) {
+
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter);
+        return factory;
+    }
 }
