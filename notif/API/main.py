adcaeab88b7a396a0360af117fa184f7433a7d5c
Patala2004 <christian.most@hotmail.com>
2025-12-27 18:40:06 +0800

    changed it to a post request so the notification message can also be sent

--- notif/API/main.py
+++ notif/API/main.py
@@ -1,24 +1,26 @@
 from fastapi import FastAPI, HTTPException, BackgroundTasks
+from pydantic import BaseModel
 import httpx
 
 app = FastAPI()
 
 EXTERNAL_API_BASE = "http://host.docker.internal:8081/user" # User api
-
-
 WECHAT_API_BASE = "http://unknown.example.com"
 
-async def send_wechat_notif(weixinId : int):
+class Message(BaseModel):
+    message: str
+
+async def send_wechat_notif(weixinId : int, message : str):
     async with httpx.AsyncClient(timeout=5.0) as client:
         try:
-            # notif_response = await client.get(f"{WECHAT_API_BASE}/{weixinId}") # For example
+            # notif_response = await client.post(f"{WECHAT_API_BASE}/{weixinId}") # For example
             # notif_response.raise_for_status()
-            print(f"Sent fake notification to weixin id {weixinId}")
+            print(f"Sent fake notification to weixin id {weixinId}: {message}")
         except httpx.HTTPError as e:
             raise HTTPException(status_code=502, detail=str(e))
 
-@app.get("/notify/{item_id}")
-async def get_item(item_id: int, background_tasks: BackgroundTasks):
+@app.post("/notify/{item_id}")
+async def get_item(item_id: int, message: Message, background_tasks: BackgroundTasks):
     async with httpx.AsyncClient(timeout=5.0) as client:
         try:
             response = await client.get(f"{EXTERNAL_API_BASE}/users/{item_id}/")
@@ -35,7 +37,7 @@ async def get_item(item_id: int, background_tasks: BackgroundTasks):
         
         # Fire-and-forget notification
         if weixinId:
-            background_tasks.add_task(send_wechat_notif, weixinId)
+            background_tasks.add_task(send_wechat_notif, weixinId, message.message)
             
 
     return {
