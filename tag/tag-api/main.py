31633b81235296d0669719f7299f9ac9b259221d
Pablo Macías <pablomaciasbrad@gmail.com>
2025-12-30 01:38:29 +0800

    add delete tag endpoint

--- tag/tag-api/main.py
+++ tag/tag-api/main.py
@@ -13,3 +13,14 @@ def tag(post_id: int):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.delete("/tag")
+def tag(post_id: int):
+    try:
+        producer.publish_to_rabbit(post_id=post_id)
+
+        return {"status": "ok"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
